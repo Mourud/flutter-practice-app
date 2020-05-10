c56267cf8b60a6c361319a22d1284c88b1233a3d
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2020-05-09 19:47:21 -0700

    tested row widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,19 +15,37 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.tealAccent,
       ),
-      body: Container(
-        padding: EdgeInsets.all(100),
-        margin: EdgeInsets.all(100),
-        color: Colors.blueGrey,
-        child: Text('HELLO'),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {},
+            color: Colors.teal,
+            child: Text('WHAT'),
+          ),
+          RaisedButton(
+            onPressed: (){},
+            color: Colors.blueGrey,
+            child: Text('IS'),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            color: Colors.teal,
+            child: Text('THIS!',
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text(
           'click',
-          style: TextStyle(color: Colors.black87),
+          style: TextStyle(
+              fontSize: 20,
+              color: Colors.black87),
         ),
-        backgroundColor: Colors.tealAccent,
+        backgroundColor: Colors.teal,
       ),
     );
   }
